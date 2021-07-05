8f19a5729b12ffe271aea9cf0c05c3ea2ae2de4e
Natsuki221 <76874939+Natsuki221@users.noreply.github.com>
2021-07-05 22:48:58 +0800

    Add files via upload

--- BMI.py
+++ BMI.py
@@ -2,9 +2,8 @@ height = input('身高（公尺）')
 weight = input('體重（公斤）') 
 height = float(height)
 weight = float(weight)
-def calculate_bmi_number():
-    bmi_value = weight/height**2
-    print('你的 BMI 指數為：{} {}'.format(bmi_value, get_bmi_status_description(bmi_value)))
+bmi_value = weight/height**2
+    
 def get_bmi_status_description(bmi_value):
     if bmi_value < 18.5:
         return '體重過輕囉，多吃點！'
@@ -12,3 +11,5 @@ def get_bmi_status_description(bmi_value):
         return '體重剛剛好，繼續保持！'
     elif bmi_value >= 24 :
         return '體重有點過重囉，少吃多運動！'
+
+print('你的 BMI 指數為：{} {}'.format(bmi_value, get_bmi_status_description(bmi_value)))
